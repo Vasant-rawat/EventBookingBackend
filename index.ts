2ccfc9d736eac7e3d5171b258b2f89321e785f66
Vasant-rawat <basantrawat9268@gmail.com>
2026-08-12 21:54:01 +0530

    initial project setup

---
+++ index.ts
@@ -0,0 +1,29 @@
+
+
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/": () => new Response('Bun!'),
+  }
+});
+
+console.log(`Listening on ${server.url}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
